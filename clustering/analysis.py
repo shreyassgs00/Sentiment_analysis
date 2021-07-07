5523547861ebfe1216431f3e721a5dc683e7f91a
shreyassgs00 <shreklife00@gmail.com>
2021-07-07 18:06:48 +0530

    Finished clustering

--- clustering/analysis.py
+++ clustering/analysis.py
@@ -1,6 +1,7 @@
 import nltk
 import pandas as pd
 import numpy as np
+import regex as re
 
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -11,6 +12,9 @@ from wordcloud import WordCloud
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.decomposition import PCA
+from sklearn.metrics import homogeneity_score
 
 data = pd.read_csv('datasets/AMD.csv')
 stopwords_model = stopwords.words('english')
@@ -20,6 +24,8 @@ wordcloud = WordCloud(stopwords=stopwords_model).generate(text)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
+
+print(data.Headline)
 
 wordfreq = {}
 for title in data.Headline:
@@ -35,12 +41,30 @@ for title in data.Headline:
 #Counting of text
 vec = CountVectorizer(binary = False)
 vec.fit(data.Headline.tolist())
-
 output = pd.DataFrame(vec.transform(data.Headline.tolist()).toarray(), columns=sorted(vec.vocabulary_.keys()))
 display(output)
 
+#TFIDF counting
 vec = TfidfVectorizer()
 vec.fit(data.Headline.tolist())
-
 output = pd.DataFrame(vec.transform(data.Headline.tolist()).toarray(), columns=sorted(vec.vocabulary_.keys()))
+features = vec.transform(data.Headline.tolist())
 display(output)
+print(features)
+
+class_clusters = MiniBatchKMeans(n_clusters = 5)
+class_clusters.fit(features)
+
+pca = PCA(n_components = 2)
+reduced_features = pca.fit_transform(features.toarray())
+reduced_cluster_centers = pca.transform(class_clusters.cluster_centers_)
+
+plt.scatter(reduced_features[:,0], reduced_features[:,1], c=class_clusters.predict(features))
+plt.scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:,1], marker='x', s=150, c='b')
+plt.show()
+
+score_homogeneity = homogeneity_score(data.Headline, class_clusters.predict(features))
+print(score_homogeneity)
+
+
+#Nearest neighbour search
