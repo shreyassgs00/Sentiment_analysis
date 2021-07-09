72fea3104fa6eae863dd6a77fbba1d7bc82a028c
nichol-dsouza <dsouzanichol@gmail.com>
2021-07-09 18:03:08 +0530

    refactored clustered code

--- clustering/analysis.py
+++ clustering/analysis.py
@@ -1,81 +1,90 @@
-import nltk
 import pandas as pd
-import numpy as np
-import regex as re
-
+import nltk
+#import os
+#import sys
+#import regex as re
+#import numpy as np
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
-from IPython.display import display
+#from IPython.display import display
 
 from wordcloud import WordCloud
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.decomposition import PCA
 from sklearn.metrics import homogeneity_score
 from sklearn.neighbors import NearestNeighbors
 
-data = pd.read_csv('datasets/AMD.csv')
-stopwords_model = stopwords.words('english')
-text = "".join(title for title in data.Headline)
-wordcloud = WordCloud(stopwords=stopwords_model).generate(text)
 
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
+def wordcloudgenerator(text):
+    stopwords_model = stopwords.words('english') 
+    wordcloud = WordCloud(stopwords=stopwords_model).generate(text)
+    return wordcloud
 
-print(data.Headline)
 
-wordfreq = {}
-for title in data.Headline:
-    words = title.split(' ')
-    for word in words:
-        if word in wordfreq:
-            wordfreq[word] +=1
-        else:
-            wordfreq[word] = 1
+def findwordfreq(data):
+    wordfreq = {}
+    for title in data:
+        words = title.split(' ')
+        for word in words:
+            if word in wordfreq:
+                wordfreq[word] +=1
+            else:
+                wordfreq[word] = 1
+    return wordfreq
 
-#vocabulary = sorted(set(word for sentence in data.Headline for word in sentence.split()))
+def textcounter_countvectorizer(data):
+    vec = CountVectorizer(binary = False)
+    vec.fit(data.Headline.tolist())
+    output = pd.DataFrame(vec.transform(data.Headline.tolist()).toarray(), columns=sorted(vec.vocabulary_.keys()))
+    return output
 
-#Counting of text
-vec = CountVectorizer(binary = False)
-vec.fit(data.Headline.tolist())
-output = pd.DataFrame(vec.transform(data.Headline.tolist()).toarray(), columns=sorted(vec.vocabulary_.keys()))
-display(output)
+def textcounter_tfidf(data):
+    vec = TfidfVectorizer()
+    vec.fit(data.Headline.tolist())
+    output = pd.DataFrame(vec.transform(data.Headline.tolist()).toarray(), columns=sorted(vec.vocabulary_.keys()))
+    features = vec.transform(data.Headline.tolist())
+    return (output,features)
 
-#TFIDF counting
-vec = TfidfVectorizer()
-vec.fit(data.Headline.tolist())
-output = pd.DataFrame(vec.transform(data.Headline.tolist()).toarray(), columns=sorted(vec.vocabulary_.keys()))
-features = vec.transform(data.Headline.tolist())
-display(output)
-print(features)
+def intensityanalyzer(data):
+    results = []
 
-class_clusters = MiniBatchKMeans(n_clusters = 5)
-class_clusters.fit(features)
+    for headline in data['Headline']:
+        pol_score = SIA().polarity_scores(headline) # run analysis
+        pol_score['headline'] = headline # add headlines for viewing
+        results.append(pol_score)
 
-pca = PCA(n_components = 2)
-reduced_features = pca.fit_transform(features.toarray())
-reduced_cluster_centers = pca.transform(class_clusters.cluster_centers_)
+    return results
 
-plt.scatter(reduced_features[:,0], reduced_features[:,1], c=class_clusters.predict(features))
-plt.scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:,1], marker='x', s=150, c='b')
-plt.show()
+def cluster(features,data):
+    class_clusters = MiniBatchKMeans(n_clusters = 5)
+    class_clusters.fit(features)
+    pca=PCA(n_components = 2)
+    reduced_features = pca.fit_transform(features.toarray())
+    reduced_cluster_centers = pca.transform(class_clusters.cluster_centers_)
+    
+    plt.scatter(reduced_features[:,0], reduced_features[:,1], c=class_clusters.predict(features))
+    plt.scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:,1], marker='x', s=150, c='b')
+    plt.show()
+    
+    score_homogeneity = homogeneity_score(data, class_clusters.predict(features))
+    return(score_homogeneity)
 
-score_homogeneity = homogeneity_score(data.Headline, class_clusters.predict(features))
-print(score_homogeneity)
-
-#Nearest neighbour search
-knn = NearestNeighbors(n_neighbors=10, metric='cosine')
-knn.fit(features)
-distance, neighbour = knn.kneighbors(features, n_neighbors=2, return_distance=True) 
-for i in data.Headline.tolist():
-    for input_text, distances, neighbors in zip(i, distance, neighbour):
-        print("Input text = ", input_text[:200], "\n")
-        for dist, neighbor_idx in zip(distances, neighbors):
-            print("Distance = ", dist, "Neighbor idx = ", neighbor_idx)
-            print("-"*200)
-    print("="*200)
-    print()
+def near_neighbour(features,data):
+    knn = NearestNeighbors(n_neighbors=10, metric='cosine')
+    knn.fit(features)
+    distance, neighbour = knn.kneighbors(features, n_neighbors=2, return_distance=True) 
+    for i in data:
+        for input_text, distances, neighbors in zip(i, distance, neighbour):
+            print("Input text = ", input_text[:200], "\n")
+            for dist, neighbor_idx in zip(distances, neighbors):
+                print("Distance = ", dist, "Neighbor idx = ", neighbor_idx)
+                print("-"*20)
+        print("="*20)
+        print()
--- clustering/main.py
+++ clustering/main.py
@@ -1,14 +1,52 @@
 import pandas as pd
-import nltk
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+#import nltk
+#import os
+#import sys
+#import numpy as np
+import matplotlib.pyplot as plt
 
-data = pd.read_csv("datasets/AMD.csv")
 
-results = []
+import analysis
 
-for headline in data['Headline']:
-    pol_score = SIA().polarity_scores(headline) # run analysis
-    pol_score['headline'] = headline # add headlines for viewing
-    results.append(pol_score)
-print(results)
+
+if __name__ == '__main__':
+	data = pd.read_csv("AMD.csv")
+	text = "".join(title for title in data.Headline)
+	
+	#stop words generator and word cloud
+
+	wordcloud=analysis.wordcloudgenerator(text)
+	'''
+	plt.imshow(wordcloud, interpolation='bilinear')
+	plt.axis("off")
+	plt.show()
+	'''
+	#find word frequency
+	
+	wordfreq=analysis.findwordfreq(data.Headline)
+
+	#print(wordfreq)
+	
+	#Counting of text
+	
+	out_vectorizer=analysis.textcounter_countvectorizer(data)
+	#print(out_vectorizer)
+
+	out_tfidf,feature_tfidf=analysis.textcounter_tfidf(data)
+	#print(feature_tfidf)
+	#print(out_tfidf)
+	
+	#K-MEANS CLUSTERING
+	
+	score_homogenity=analysis.cluster(feature_tfidf,data.Headline)
+	#print(score_homogenity)
+
+	#Nearest Neighbour search
+	analysis.near_neighbour(feature_tfidf,data.Headline.tolist())
+	
+
+	#intensity analyzer
+
+	results = analysis.intensityanalyzer(data)
+	#print(results)
+
